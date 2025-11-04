67a337bbc90992a846156d3d30df645d213df663
Fernando <fernandop@pmgintelligence.com>
2025-11-04 14:16:46 -0500

    another restoration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hangman-game/',
-  plugins: [react()]
-})
+	base: "/hangman-game/",
+	plugins: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+	],
+});
