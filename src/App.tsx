7ea43d3d0e8ce7194d20b9f108415959fcf323bc
Fernando <fernandop@pmgintelligence.com>
2025-11-04 18:11:37 -0500

    quick update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { useState, useEffect, useCallback } from "react";
-import words from "./wordList.json";
+// import words from "./wordList.json";
+import wordCategoriesList from "./wordCategoriesList.json";
 import ForcaDrawing from "./components/ForcaDrawing";
 import ForcaWord from "./components/ForcaWord";
 import ForcaKeyboard from "./components/ForcaKeyboard";
 import "./App.css";
 
 function getWord() {
-	const randomIndex = Math.floor(Math.random() * words.length);
-	return words[randomIndex].toUpperCase();
+	const allWords = Object.values(wordCategoriesList).flat();
+	const randomIndex = Math.floor(Math.random() * allWords.length);
+	return allWords[randomIndex].toUpperCase();
 }
 
 function App() {
-	// Estado para a palavra a ser adivinhada
 	const [wordToGuess, setWordToGuess] = useState(() => {
 		// Seleciona uma palavra aleatória da lista
 		return getWord();
@@ -86,6 +87,24 @@ function App() {
 				<div style={{ fontSize: "2rem", textAlign: "center" }}>
 					{isWinner && "Parabéns! Você ganhou!"}
 					{isLoser && `Que pena! A palavra era: ${wordToGuess}`}
+				</div>
+				<div style={{ fontSize: "1.5rem", textAlign: "center" }}>
+					<button
+						style={{
+							backgroundColor: "#4CAF50",
+							color: "white",
+							padding: "10px 20px",
+							border: "none",
+							borderRadius: "5px",
+							cursor: "pointer",
+						}}
+						onClick={() => {
+							setGuessedLetters([]);
+							setWordToGuess(getWord());
+						}}
+					>
+						Play New Game or Press Enter
+					</button>
 				</div>
 				<ForcaDrawing numberOfGuesses={incorrectLetters.length} />
 				<ForcaWord
